39df01704e62055d491d74f2f3f766471b0b6464
https://github.com/reardonp <Reardopb@IL-PC-142.uccc.uc.edu>
2015-02-24 20:16:45 -0500

    Added private int reardonp to line 15

--- src/hangmanGUI/Config.java
+++ src/hangmanGUI/Config.java
@@ -11,6 +11,7 @@ import hangmanPackage.Preferences;
 import java.awt.Color;
 
 public class Config {
+	
 	private int reardonp;
 	private int baseHeight, baseWidth;
 	private int poleHeight, poleWidth;
