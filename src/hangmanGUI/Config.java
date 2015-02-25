75590cd5924679bb69280bec7dacd8495df4cdb5
https://github.com/wrightdb <xer087@gmail.com>
2015-02-24 17:04:44 -0800

    Bomb Diggity Swiggaty Swoody

--- src/hangmanGUI/Config.java
+++ src/hangmanGUI/Config.java
@@ -11,6 +11,7 @@ import hangmanPackage.Preferences;
 import java.awt.Color;
 
 public class Config {
+	private int wrightdbIstheBombdiggity;
 	private int baseHeight, baseWidth;
 	private int poleHeight, poleWidth;
 	private int armHeight, armWidth;
